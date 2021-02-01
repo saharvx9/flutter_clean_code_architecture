62e1e362b8fc402bceed1d67a9f631e8f991bcdf
Sahar Vanunu <sahar.x5x@gmail.com>
2021-02-01 18:48:55 +0200

    fix bug

--- lib/application/api_configuration.dart
+++ lib/application/api_configuration.dart
@@ -1,4 +1,4 @@
 class ApiConfiguration{
-  // static const BASE_URL = "http://10.0.0.27:5000/api/";
-  static const BASE_URL = "http://192.168.86.66:5000/api/";
+  static const BASE_URL = "http://10.0.0.27:5000/api/";
+  // static const BASE_URL = "http://ip_router:5000/api/";
 }
